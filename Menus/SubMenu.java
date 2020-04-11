48962477b8006c319fbd0b03a7c7e2eae643e869
Daetwen <62154512+Daetwen@users.noreply.github.com>
2020-04-11 10:16:27 +0300

    Update SubMenu.java

--- Menus/SubMenu.java
+++ Menus/SubMenu.java
@@ -3,12 +3,49 @@ package sample;
 import javafx.scene.layout.VBox;
 
 public class SubMenu extends VBox {
+
+    /**
+     * Конструктор - создание нового объекта, являющегося контейнером для объектов-кнопок.
+     * @param items - объекты класса MenuItem, являющиеся кнопками.
+     */
     public SubMenu(MenuItem...items){
-        setSpacing(5);
-        setTranslateX(10);
-        setTranslateY(300);
-        for(MenuItem item: items){
-            getChildren().addAll(item);
+        try{
+            setSpacing(5);
+            setTranslateX(10);
+            setTranslateY(375);
+            for(MenuItem item: items){
+                getChildren().addAll(item);
+            }
         }
+        catch(Exception ex){
+            ex.getLocalizedMessage();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Конструктор - создание нового объекта, являющегося контейнером для объектов-кнопок.
+     * @param X - координата для отображения объекта по вертикали
+     * @param Y - координата для отображения объекта по горизонтали.
+     * @param items - объекты класса MenuItem, являющиеся кнопками.
+     * @throws IllegalArgumentException, если координаты для отображения меньше 0.
+     */
+    public SubMenu(int X,int Y,MenuItem...items){
+       try{
+           setSpacing(5);
+           if(X < 0)
+               throw new IllegalArgumentException("Число положения меню по горизонтали меньше 0.");
+           else setTranslateX(X);
+           if(Y < 0)
+               throw new IllegalArgumentException("Число положения меню по вертикали меньше 0.");
+           else setTranslateY(Y);
+           for(MenuItem item: items){
+               getChildren().addAll(item);
+           }
+       }
+       catch(Exception ex){
+           ex.getLocalizedMessage();
+           System.exit(1);
+       }
     }
 }
