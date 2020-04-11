44f71615c23bcdf618bd90d9561b108f6a655438
Daetwen <62154512+Daetwen@users.noreply.github.com>
2020-04-11 10:13:46 +0300

    Update MenuItem.java

--- Menus/MenuItem.java
+++ Menus/MenuItem.java
@@ -7,30 +7,45 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class MenuItem extends StackPane {
-    public MenuItem(String name){
-        Rectangle rect = new Rectangle(250,40, Color.BLACK);
-        Text text = new Text(name);
-        text.setFill(Color.WHITE);
-        text.setFont(Font.font("TimesNewRoman", FontWeight.BOLD, 9));
 
-        this.setAlignment(Pos.CENTER);
-        getChildren().addAll(rect,text);
-        FillTransition trans = new FillTransition(Duration.seconds(0.5),rect);
-        setOnMouseEntered(event->{
-            trans.setFromValue(Color.BLACK);
-            trans.setToValue(Color.DARKGOLDENROD);
-            trans.setCycleCount(Animation.INDEFINITE);
-            trans.setAutoReverse(true);
-            trans.play();
-        });
-        setOnMouseExited(event->{
-            trans.stop();
-            rect.setFill(Color.BLACK);
-        });
+    /**
+     * Конструктор - создание нового объекта с определенными значениями.
+     * @param name - текст, который будет отображён на объекте.
+     * @param sizeX - размер объекта по вертикали.
+     * @param sizeY - размер объекта по вертикали.
+     * @throws IllegalArgumentException, если размер объекта меньше нуля.
+     */
+    public MenuItem(String name, int sizeY, int sizeX){
+        try{
+            if(sizeX < 0 || sizeY < 0)
+                throw new IllegalArgumentException("Одно из чисел размера кнопки меню меньше 0.");
+            Rectangle rect = new Rectangle(sizeY,sizeX, Color.BLACK);
+            Text text = new Text(name);
+            text.setFill(Color.WHITE);
+            text.setFont(Font.loadFont("file:src/sample/MR_HOUSEBROKENROUGHG.ttf",20.0D));
+
+            this.setAlignment(Pos.CENTER);
+            getChildren().addAll(rect,text);
+            FillTransition trans = new FillTransition(Duration.seconds(0.5),rect);
+            setOnMouseEntered(event->{
+                trans.setFromValue(Color.BLACK);
+                trans.setToValue(Color.DARKGOLDENROD);
+                trans.setCycleCount(Animation.INDEFINITE);
+                trans.setAutoReverse(true);
+                trans.play();
+            });
+            setOnMouseExited(event->{
+                trans.stop();
+                rect.setFill(Color.BLACK);
+            });
+        }
+        catch(Exception ex){
+            ex.getLocalizedMessage();
+            System.exit(1);
+        }
     }
 }
