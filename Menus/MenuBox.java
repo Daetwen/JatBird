a1c0f69ce6407db5a1b2e004a0a9d8a520989030
Daetwen <62154512+Daetwen@users.noreply.github.com>
2020-04-11 10:11:30 +0300

    Update MenuBox.java

--- Menus/MenuBox.java
+++ Menus/MenuBox.java
@@ -3,15 +3,39 @@ package sample;
 import javafx.scene.layout.Pane;
 
 public class MenuBox extends Pane {
+
+    /** Объект-контейнер набора объектов-кнопок*/
     static SubMenu subMenu;
+
+    /**
+     * Конструктор - создание нового объекта с определенными значениями.
+     * @param subMenu - объекты класса SubMenu, являющегося контейнером объектов-кнопок.
+     */
     public MenuBox(SubMenu subMenu){
-        MenuBox.subMenu = subMenu;
-        setVisible(true);
-        getChildren().add(subMenu);
+        try{
+            MenuBox.subMenu = subMenu;
+            setVisible(true);
+            getChildren().add(subMenu);
+        }
+        catch(Exception ex){
+            ex.getLocalizedMessage();
+            System.exit(1);
+        }
     }
+
+    /**
+     * Функция установки нужного контейнера объектов-кнопок вместо старого.
+     * @param subMenu - объекты класса SubMenu, являющегося контейнером объектов-кнопок.
+     */
     public void setSubMenu(SubMenu subMenu){
-        getChildren().remove(MenuBox.subMenu);
-        MenuBox.subMenu = subMenu;
-        getChildren().add(MenuBox.subMenu);
+        try{
+            getChildren().remove(MenuBox.subMenu);
+            MenuBox.subMenu = subMenu;
+            getChildren().add(MenuBox.subMenu);
+        }
+        catch(Exception ex){
+            ex.getLocalizedMessage();
+            System.exit(1);
+        }
     }
 }
