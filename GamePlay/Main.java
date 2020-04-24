2e35d60c2bf593a1ca47264d3963b331d39e2827
Daetwen <62154512+Daetwen@users.noreply.github.com>
2020-04-24 18:38:43 +0300

    Update Main.java

--- GamePlay/Main.java
+++ GamePlay/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -34,23 +35,42 @@ public class Main extends Application {
     /** Поле игровых компонентов */
     public static Pane gameRoot = new Pane();
     /** Объект класса игрового персонажа */
-    Person person = new Person();
+    static Person person = new Person();
     /** Коллекция игровых объектов препятствий-лазеров */
     public static ArrayList<Obstacles> obstacles = new ArrayList<>();
     /** Коллекция игровых объектов монет */
     public static ArrayList<MyCoin> coins = new ArrayList<>();
     /** Коллекция игровых объектов бонусов */
     public static ArrayList<LocalBonus> bonuses = new ArrayList<>();
+    /** Коллекция игровых объектов бонусов-вторых шансов */
+    public static ArrayList<SecondChansBonus> secondChanses = new ArrayList<>();
     /** Счётчик достигнутого результата */
     public static double score = 0;
     /** Счётчик монет в наличии*/
     public static int coin_score = 0;
     /** Переменная силы прыжка по Х для сложности игры */
-    double ComplexityX;
+    public static double ComplexityX;
     /** Переменная силы прыжка по Y для сложности игры */
-    double ComplexityY;
+    public static double ComplexityY;
     /** Таймер для обновления постоянного вызова обновления положения персонажа */
     AnimationTimer timer;
+    /** Коллекция объектов сохранений */
+    public static ArrayList<SaveInfo> saves2 = new ArrayList<>();
+    /** Массив стрингов с набором информации для таблицы рекордов */
+    String[] SaveText = this.SaveMessage();
+    /** Объекты класса  MenuItem, являющиеся кнопками-сообщениями в таблице рекордов*/
+    MenuItem save1 = new MenuItem(SaveText[0],580,40, Pos.CENTER_LEFT);
+    MenuItem save2 = new MenuItem(SaveText[1],580,40, Pos.CENTER_LEFT);
+    MenuItem save3 = new MenuItem(SaveText[2],580,40, Pos.CENTER_LEFT);
+    MenuItem save4 = new MenuItem(SaveText[3],580,40, Pos.CENTER_LEFT);
+    MenuItem save5 = new MenuItem(SaveText[4],580,40, Pos.CENTER_LEFT);
+    MenuItem save6 = new MenuItem(SaveText[5],580,40, Pos.CENTER_LEFT);
+    MenuItem save7 = new MenuItem(SaveText[6],580,40, Pos.CENTER_LEFT);
+    MenuItem save8 = new MenuItem(SaveText[7],580,40, Pos.CENTER_LEFT);
+    MenuItem save9 = new MenuItem(SaveText[8],580,40, Pos.CENTER_LEFT);
+    MenuItem save10 = new MenuItem(SaveText[9],580,40, Pos.CENTER_LEFT);
+    /** Объект класса  MenuItem, являющиеся кнопккой возврата в главное меню*/
+    MenuItem saveback = new MenuItem("НАЗАД",580,40);
 
     /**
      * Функция добавления картинки лазера, положения лазера и добавления объекта лазера в соответствующую коллекцию.
@@ -144,6 +164,14 @@ public class Main extends Application {
 
                 gameRoot.getChildren().addAll(bonus);                //Добавление бонуса для отображения
             }
+            for(int i = 0;i < 3;i++){                                //Цикл создания и спавна бонуса
+                SecondChansBonus second = new SecondChansBonus(15);
+                second.setTranslateX(i*1000+1400);
+                second.setTranslateY(Math.random()*420);
+                secondChanses.add(second);
+
+                gameRoot.getChildren().addAll(second);                //Добавление бонуса для отображения
+            }
             int positionY = 50;
             int positionX = 600;
             for(int i = 1;i <= 25;i++) {                            //Цикл создания и спавна монеток
@@ -152,7 +180,6 @@ public class Main extends Application {
                     MyCoin coin = new MyCoin(13);
                     coin.setTranslateX(positionX + y*100);
                     coin.setTranslateY(positionY);
-                    System.out.println(positionY);
                     coins.add(coin);
                     gameRoot.getChildren().addAll(coin);               //Добавление монеток для отображения
                 }
@@ -254,11 +281,11 @@ public class Main extends Application {
                     return;
                 }
                 if(keyCode.equals(KeyCode.F5)){
-                    Save();
+                    Save("Save.txt");
                     return;
                 }
                 if(keyCode.equals(KeyCode.R)){
-                    Reload();
+                    Reload("Save.txt");
                     isSaved = true;
                     return;
                 }
@@ -279,9 +306,14 @@ public class Main extends Application {
                 @Override
                 public void handle(long now) {
                     update();
-                    if(person.count == true){
+                    if(person.count == true && isSaved == true){
+                        Reload("Save.txt");
+                        isSaved = false;
+                    }
+                    if(person.count == true && isSaved == false){
                         IsDead(primaryStage, menu,person.count);
                         timer.stop();
+                        IsRecord();
                         restart();
                     }
                 }
@@ -314,6 +346,7 @@ public class Main extends Application {
                 primaryStage.show();
                 timer.stop();
                 restart();
+                SaveText = this.SaveMessage();
             });
             game.setOnMouseClicked(event->{Play(primaryStage,menu,ComplexityX,ComplexityY);});
         }
@@ -337,6 +370,7 @@ public class Main extends Application {
             person.count = false;
             appRoot = new Pane();
             gameRoot = new Pane();
+
         }
         catch(Exception ex){
             ex.getLocalizedMessage();
@@ -347,6 +381,9 @@ public class Main extends Application {
     /**
      * Функция создания начального меню игры и ивентов начала игрового процесса с заданным уровнем сложности.
      * @param primaryStage - объект класса-контейнера для всех элементов.
+     * @see "class MenuBox"
+     * @see "class MenuItem"
+     * @see "class SubMenu"
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -361,7 +398,7 @@ public class Main extends Application {
             MenuItem newGame = new MenuItem("НОВАЯ ИГРА",250,40);
             MenuItem reloadGame = new MenuItem("ПОСЛЕДНЕЕ СОХРАНЕНИЕ",250,40);
             MenuItem options = new MenuItem("НАСТРОЙКИ",250,40);
-            MenuItem records= new MenuItem("РЕКОРДЫ",250,40);
+            MenuItem records = new MenuItem("РЕКОРДЫ",250,40);
             MenuItem exit = new MenuItem("ВЫХОД",250,40);
             SubMenu mainMenu = new SubMenu(newGame, reloadGame, options, records, exit);
 
@@ -385,15 +422,32 @@ public class Main extends Application {
             //Переходы из меню в разные разделы и обратно по нажатии нужного пункта
             newGame.setOnMouseClicked(event->menuBox.setSubMenu(newGameMenu));
             options.setOnMouseClicked(event->menuBox.setSubMenu(optionsMenu));
+            records.setOnMouseClicked(event->{
+                SaveText = SaveMessage();
+                RecordsMap();
+                SubMenu SaveMenu = new SubMenu(10,107, save1, save2, save3, save4, save5, save6, save7, save8, save9, save10,saveback);
+                menuBox.setSubMenu(SaveMenu);
+                //SortMenu(menu_pane);
+            });
+            saveback.setOnMouseClicked((event->menuBox.setSubMenu(mainMenu)));
             exit.setOnMouseClicked(event-> System.exit(0));
             exit2.setOnMouseClicked(event->menuBox.setSubMenu(mainMenu));
             exit3.setOnMouseClicked(event->menuBox.setSubMenu(mainMenu));
             menu_pane.getChildren().add(menuBox);
 
             reloadGame.setOnMouseClicked(event->{
-                Reload();
+                Reload("Save.txt");
                 Play(primaryStage,menu,ComplexityX ,ComplexityY);
             });
+            save1.setOnMouseClicked(event->{
+                StartFromRecords(0);
+                Play(primaryStage,menu,ComplexityX ,ComplexityY);
+            });
+            save10.setOnMouseClicked(event->{
+                StartFromRecords(9);
+                Play(primaryStage,menu,ComplexityX ,ComplexityY);
+            });
+
 
             //Запуск игры с установкой параметров сложности
             one.setOnMouseClicked(event->{
@@ -450,8 +504,8 @@ public class Main extends Application {
             text2.setTranslateY(270);
             text1.setFill(Color.WHITESMOKE);
             text2.setFill(Color.WHITESMOKE);
-            text1.setFont(Font.loadFont("file:src/sample/MR_HOUSEBROKENROUGHG.ttf",23.0D));
-            text2.setFont(Font.loadFont("file:src/sample/MR_HOUSEBROKENROUGHG.ttf",23.0D));
+            text1.setFont(Font.loadFont("file:src/sample/LJK_DOWNCOME.otf",23.0D));
+            text2.setFont(Font.loadFont("file:src/sample/LJK_DOWNCOME.otf",23.0D));
             appRoot.getChildren().addAll(rect,text1,text2);
         }
         catch(Exception ex){
@@ -462,9 +516,10 @@ public class Main extends Application {
 
     /**
      * Функция сохранения в файл основных геимплеиных полей.
+     * @param NameOfFile - название файла для работы с сохранениями
      */
-    public void Save() {
-        try(FileWriter writer = new FileWriter("Save.txt", false))
+    public static void Save(String NameOfFile) {
+        try(FileWriter writer = new FileWriter(NameOfFile, false))
         {
             String sScore = Double.toString(score);
             String sCoinScore = Integer.toString(coin_score);
@@ -480,6 +535,7 @@ public class Main extends Application {
             writer.write(sComplexityX + "\r\n");
             writer.write(sComplexityY + "\r\n");
             writer.write( sLaserStation + "\r\n");
+            writer.write( "\r\n");
             writer.flush();
             writer.close();
         }
@@ -490,10 +546,11 @@ public class Main extends Application {
 
     /**
      * Функция чтения из файла и записи в соответсвующие поля данных.
+     * @param NameOfFile - название файла для работы с сохранениями
      */
-    public void Reload(){
-        try (BufferedReader reader = new BufferedReader(new FileReader("Save.txt"))) {
-
+    public static void Reload(String NameOfFile){
+        try (BufferedReader reader = new BufferedReader(new FileReader(NameOfFile))) {
+            person.count = false;
             String sScore, CoinScore, sPositionX, sPositionY, sComplexityX, sComplexityY;
             sScore = reader.readLine();
             CoinScore = reader.readLine();
@@ -502,6 +559,7 @@ public class Main extends Application {
             sComplexityX = reader.readLine();
             sComplexityY = reader.readLine();
             String sLaserStation = reader.readLine();
+            String newStr = reader.readLine();
             score = Double.valueOf(sScore);
             coin_score = Integer.valueOf(CoinScore);
             person.setTranslateX(Double.valueOf(sPositionX));
@@ -511,9 +569,180 @@ public class Main extends Application {
             station = Integer.valueOf(sLaserStation);
             reader.close();
         }
+        catch (IOException ex1){
+            ex1.getLocalizedMessage();
+            System.exit(1);
+        }
         catch (Exception ex){
             ex.getLocalizedMessage();
             System.exit(1);
         }
+    }
+
+    /**
+     * Функция создания рекордного сохранения, сортировки с остальными рекордами и записи таблицы рекордов в файл.
+     */
+    public void IsRecord(){
+        SaveInfo save = new SaveInfo(score,coin_score,person.getTranslateX(),person.getTranslateY(),ComplexityX,ComplexityY,station);
+        Read("Save2.txt",saves2);
+        saves2.add(save);
+        SaveInfo localsave = new SaveInfo();
+        for(int i = 1; i < saves2.size(); ++i){
+            for(int y = 0; y < saves2.size() - 1; y++){
+                if(saves2.get(y).sScore < saves2.get(y+1).sScore){
+                    Switch(localsave,saves2.get(y));
+                    Switch(saves2.get(y),saves2.get(y+1));
+                    Switch(saves2.get(y+1),localsave);
+                }
+            }
+        }
+        if(saves2.size() == 11) saves2.remove(10);
+        Write("Save2.txt",saves2);
+        SaveMessage();
+    }
+
+    /**
+     * Функция чтения параметров из одного объекта класса SaveInfo в другой.
+     * @param first - объект, в который происходит считывание
+     * @param second -  объект, из которого происходит считывание
+     */
+    public void Switch(SaveInfo first,SaveInfo second){
+        first.sScore = second.sScore;
+        first.sCoinScore = second.sCoinScore;
+        first.sPositionX = second.sPositionX;
+        first.sPositionY = second.sPositionY;
+        first.sComplexityX = second.sComplexityX;
+        first.sComplexityY = second.sComplexityY;
+        first.sLaserStation = second.sLaserStation;
+    }
+
+    /**
+     * Функция записи в файл содержимого коллекции.
+     * @param name - название файла, в который происходит запись
+     * @param saves -  коллекция, из которой происходит считывание
+     */
+    public void Write(String name,ArrayList<SaveInfo> saves){
+        try(FileWriter writer = new FileWriter(name, false))
+        {
+            int i = 0;
+            while(saves.size() != i) {
+                String sScore = Double.toString(saves.get(i).sScore);
+                String sCoinScore = Integer.toString(saves.get(i).sCoinScore);
+                String sPositionX = Double.toString(saves.get(i).sPositionX);
+                String sPositionY = Double.toString(saves.get(i).sPositionY);
+                String sComplexityX = Double.toString(saves.get(i).sComplexityX);
+                String sComplexityY = Double.toString(saves.get(i).sComplexityY);
+                String sLaserStation = Integer.toString(saves.get(i).sLaserStation);
+                writer.write(sScore + "\r\n");
+                writer.write(sCoinScore + "\r\n");
+                writer.write(sPositionX + "\r\n");
+                writer.write(sPositionY + "\r\n");
+                writer.write(sComplexityX + "\r\n");
+                writer.write(sComplexityY + "\r\n");
+                writer.write(sLaserStation + "\r\n");
+                writer.write("=============================================" + "\r\n");
+                writer.flush();
+                i++;
+            }
+            writer.close();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Функция чтения из файла в коллекцию.
+     * @param name - название файла, из которого происходит считывание
+     * @param saves -  коллекция, в которую происходит считывание
+     */
+    public void Read(String name,ArrayList<SaveInfo> saves){
+        try (BufferedReader reader = new BufferedReader(new FileReader(name))) {
+            saves.clear();
+            String sScore, sCoinScore, sPositionX, sPositionY, sComplexityX, sComplexityY, sLaserStation, probels;
+            int i = 1;
+            do{
+                sScore = reader.readLine();
+                if((sCoinScore = reader.readLine()) == null)break;
+                sPositionX = reader.readLine();
+                sPositionY = reader.readLine();
+                sComplexityX = reader.readLine();
+                sComplexityY = reader.readLine();
+                sLaserStation = reader.readLine();
+                probels = reader.readLine();
+                SaveInfo localsave = new SaveInfo();
+                localsave.sScore = Double.valueOf(sScore);
+                localsave.sCoinScore = Integer.valueOf(sCoinScore);
+                localsave.sPositionX = Double.valueOf(sPositionX);
+                localsave.sPositionY = Double.valueOf(sPositionY);
+                localsave.sComplexityX = Double.valueOf(sComplexityX);
+                localsave.sComplexityY = Double.valueOf(sComplexityY);
+                localsave.sLaserStation = Integer.valueOf(sLaserStation);
+                saves.add(localsave);
+                i++;
+            }while(true);
+        }
+        catch (Exception ex){
+            System.out.println("Error in method Read().");
+            System.out.println(ex.getLocalizedMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Функция записи массива стрингов информации сохранений из файла
+     * @return String[] - массив строк с информацие для таблицы рекордов
+     */
+    public String[] SaveMessage(){
+        String[] SaveText = new String[10];
+        saves2.clear();
+        Read("Save2.txt",saves2);
+        for(int i = 0;i < saves2.size();i++){
+            SaveText[i] = "                 " + (i+1) + " )   ";
+            if(saves2.get(i).sComplexityX == 1) SaveText[i] += "Лёгкий уровень. ";
+            else if(saves2.get(i).sComplexityX == 1.2) SaveText[i] += "Средний уровень. ";
+            else if(saves2.get(i).sComplexityX == 1.5) SaveText[i] += "Тяжёлый уровень. ";
+            else if(saves2.get(i).sComplexityX == 2) SaveText[i] += "Уровень для психов. ";
+            SaveText[i] += "Счёт: " + saves2.get(i).sScore + " | Монеты: " + saves2.get(i).sCoinScore;
+        }
+        return SaveText;
+    }
+
+    /**
+     * Функция получения из файла и записи в соответствующие поля сохранённой информации
+     * @param index - номер сохранения
+     */
+    public void StartFromRecords(int index){
+        try{
+            saves2.clear();
+            Read("Save2.txt",saves2);
+            this.score = saves2.get(index).sScore;
+            this.coin_score = saves2.get(index).sCoinScore;
+            person.setTranslateX(saves2.get(index).sPositionX);
+            person.setTranslateY(saves2.get(index).sPositionY);
+            this.ComplexityX = saves2.get(index).sComplexityX;
+            this.ComplexityY = saves2.get(index).sComplexityY;
+            station = saves2.get(index).sLaserStation;
+        }
+        catch (Exception ex){
+            ex.getLocalizedMessage();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Функция записи в соответствующие ячейки значений из массива информации
+     */
+    public void RecordsMap(){
+        save1 = new MenuItem(SaveText[0],580,40, Pos.CENTER_LEFT);
+        save2 = new MenuItem(SaveText[1],580,40, Pos.CENTER_LEFT);
+        save3 = new MenuItem(SaveText[2],580,40, Pos.CENTER_LEFT);
+        save4 = new MenuItem(SaveText[3],580,40, Pos.CENTER_LEFT);
+        save5 = new MenuItem(SaveText[4],580,40, Pos.CENTER_LEFT);
+        save6 = new MenuItem(SaveText[5],580,40, Pos.CENTER_LEFT);
+        save7 = new MenuItem(SaveText[6],580,40, Pos.CENTER_LEFT);
+        save8 = new MenuItem(SaveText[7],580,40, Pos.CENTER_LEFT);
+        save9 = new MenuItem(SaveText[8],580,40, Pos.CENTER_LEFT);
+        save10 = new MenuItem(SaveText[9],580,40, Pos.CENTER_LEFT);
     }
 }
